a39f0fce8953fe5683c7465fd1e88de2a88090b3
Aaron Dandy <aaron.dandy@live.com>
2013-01-28 23:20:13 -0700

    adding some tests from the old legacy XML docs to the new stuff

--- src/DandyDoc.Core/XmlDoc/XmlDocMember.cs
+++ src/DandyDoc.Core/XmlDoc/XmlDocMember.cs
@@ -14,6 +14,13 @@ namespace DandyDoc.XmlDoc
 		{
 			public XmlDocElement FirstSummary;
 			public IList<XmlDocNode> AllSummaryContents;
+			public XmlDocElement FirstReturnsSummary;
+			public IList<XmlDocNode> AllReturnsSummary;
+			public XmlDocElement FirstValueSummary;
+			public IList<XmlDocNode> AllValueSummary; 
+			public IList<XmlDocElement> RemarksElements;
+			public IList<XmlDocRefElement> ExceptionElements;
+			public IList<XmlDocNameElement> ParameterSummaries;
 		}
 
 		private readonly GutsData _guts;
@@ -29,31 +36,155 @@ namespace DandyDoc.XmlDoc
 		private GutsData CreateGuts() {
 			var result = new GutsData();
 			var summaryContents = new List<XmlDocNode>();
+			var returnsContents = new List<XmlDocNode>();
+			var valueContents = new List<XmlDocNode>();
+			var remarksElements = new List<XmlDocElement>();
+			var exceptionElements = new List<XmlDocRefElement>();
+			var parameterSummaries = new List<XmlDocNameElement>();
 			foreach (var child in Children.OfType<XmlDocElement>()) {
 				if ("SUMMARY".Equals(child.Name, StringComparison.OrdinalIgnoreCase)) {
-					if (result.FirstSummary == null)
-						result.FirstSummary = child;
-					if(child.HasChildren)
+					if (child.HasChildren) {
+						if (result.FirstSummary == null)
+							result.FirstSummary = child;
 						summaryContents.AddRange(child.Children);
+					}
+				}
+				else if ("RETURNS".Equals(child.Name, StringComparison.OrdinalIgnoreCase)) {
+					if (child.HasChildren) {
+						if (result.FirstReturnsSummary == null)
+							result.FirstReturnsSummary = child;
+						returnsContents.AddRange(child.Children);
+					}
+				}
+				else if ("VALUE".Equals(child.Name, StringComparison.OrdinalIgnoreCase)) {
+					if (child.HasChildren) {
+						if (result.FirstValueSummary == null)
+							result.FirstValueSummary = child;
+						valueContents.AddRange(child.Children);
+					}
+				}
+				else if ("REMARKS".Equals(child.Name, StringComparison.OrdinalIgnoreCase)) {
+					if (child.HasChildren)
+						remarksElements.Add(child);
+				}
+				else if ("EXCEPTION".Equals(child.Name, StringComparison.OrdinalIgnoreCase)) {
+					if(child is XmlDocRefElement)
+						exceptionElements.Add((XmlDocRefElement)child);
+				}
+				else if (child is XmlDocNameElement) {
+					if("PARAM".Equals(child.Name, StringComparison.OrdinalIgnoreCase))
+						parameterSummaries.Add((XmlDocNameElement)child);
 				}
 			}
 			result.AllSummaryContents = new ReadOnlyCollection<XmlDocNode>(summaryContents.ToArray());
+			result.AllReturnsSummary = new ReadOnlyCollection<XmlDocNode>(returnsContents.ToArray());
+			result.AllValueSummary = new ReadOnlyCollection<XmlDocNode>(valueContents.ToArray());
+			result.RemarksElements = new ReadOnlyCollection<XmlDocElement>(remarksElements.ToArray());
+			result.ExceptionElements = new ReadOnlyCollection<XmlDocRefElement>(exceptionElements.ToArray());
+			result.ParameterSummaries = new ReadOnlyCollection<XmlDocNameElement>(parameterSummaries.ToArray());
 			return result;
 		}
 
 		public XmlDocElement SummaryElement {
-			get { return _guts.FirstSummary; }
+			get {
+				Contract.Ensures(
+					Contract.Result<XmlDocElement>() == null
+					|| Contract.Result<XmlDocElement>().HasChildren
+				);
+				return _guts.FirstSummary;
+			}
 		}
 
 		public IList<XmlDocNode> SummaryContents {
 			get {
 				Contract.Ensures(Contract.Result<IList<XmlDocNode>>() != null);
+				Contract.Ensures(Contract.ForAll(Contract.Result<IList<XmlDocNode>>(), x => x != null));
 				return _guts.AllSummaryContents;
 			}
 		}
 
 		public bool HasSummaryContents {
 			get { return SummaryContents.Count > 0; }
+		}
+
+		public XmlDocElement ReturnsElement {
+			get {
+				Contract.Ensures(
+					Contract.Result<XmlDocElement>() == null
+					|| Contract.Result<XmlDocElement>().HasChildren
+				);
+				return _guts.FirstReturnsSummary;
+			}
+		}
+
+		public IList<XmlDocNode> ReturnsContents {
+			get {
+				Contract.Ensures(Contract.Result<IList<XmlDocNode>>() != null);
+				Contract.Ensures(Contract.ForAll(Contract.Result<IList<XmlDocNode>>(), x => x != null));
+				return _guts.AllReturnsSummary;
+			}
+		}
+
+		public bool HasReturnsContents {
+			get { return ReturnsContents.Count > 0; }
+		}
+
+		public XmlDocElement ValueElement {
+			get {
+				Contract.Ensures(
+					Contract.Result<XmlDocElement>() == null
+					|| Contract.Result<XmlDocElement>().HasChildren
+				);
+				return _guts.FirstValueSummary;
+			}
+		}
+
+		public IList<XmlDocNode> ValueContents {
+			get {
+				Contract.Ensures(Contract.Result<IList<XmlDocNode>>() != null);
+				Contract.Ensures(Contract.ForAll(Contract.Result<IList<XmlDocNode>>(), x => x != null));
+				return _guts.AllValueSummary;
+			}
+		}
+
+		public bool HasValueContents {
+			get { return ValueContents.Count > 0; }
+		}
+
+		public IList<XmlDocElement> RemarksElements {
+			get {
+				Contract.Ensures(Contract.Result<IList<XmlDocElement>>() != null);
+				Contract.Ensures(Contract.ForAll(Contract.Result<IList<XmlDocElement>>(), x => x != null && x.HasChildren));
+				return _guts.RemarksElements;
+			}
+		}
+
+		public bool HasRemarksElements { get { return RemarksElements.Count > 0; } }
+
+		public IList<XmlDocRefElement> ExceptionElements {
+			get {
+				Contract.Ensures(Contract.Result<IList<XmlDocRefElement>>() != null);
+				Contract.Ensures(Contract.ForAll(Contract.Result<IList<XmlDocRefElement>>(), x => x != null));
+				return _guts.ExceptionElements;
+			}
+		}
+
+		public bool HasExceptionElements { get { return ExceptionElements.Count > 0; } }
+
+		public bool HasParameterSummaries { get { return ParameterSummaries.Count > 0; } }
+
+		public IList<XmlDocNameElement> ParameterSummaries {
+			get {
+				Contract.Ensures(Contract.Result<IList<XmlDocNameElement>>() != null);
+				Contract.Ensures(Contract.ForAll(Contract.Result<IList<XmlDocNameElement>>(), x => x != null && x.HasChildren));
+				return _guts.ParameterSummaries;
+			}
+		}
+
+		public XmlDocNameElement GetParameterSummary(string parameterName) {
+			if(String.IsNullOrEmpty(parameterName)) throw new ArgumentException("Invalid parameter name.", "parameterName");
+			Contract.EndContractBlock();
+			return ParameterSummaries.FirstOrDefault(x => x.TargetName == parameterName);
 		}
 
 	}
--- tests/TestLibrary1.Test/XmlDocTests.cs
+++ tests/TestLibrary1.Test/XmlDocTests.cs
@@ -37,8 +37,15 @@ namespace TestLibrary1.Test
 		}
 
 		[Test]
+		public void read_no_docs_from_class() {
+			var docs = Docs.GetMember("T:TestLibrary1.Class1.NoDocs");
+			Assert.IsNull(docs);
+		}
+
+		[Test]
 		public void read_summary_from_class() {
 			var docs = Docs.GetMember("T:TestLibrary1.Class1");
+			Assert.IsNotNull(docs);
 			Assert.IsNotNull(docs.SummaryElement);
 			Assert.IsTrue(docs.HasSummaryContents);
 			Assert.AreEqual(1, docs.SummaryContents.Count);
@@ -46,6 +53,125 @@ namespace TestLibrary1.Test
 				"This class is just for testing and has no real use outside of generating some documentation.",
 				((XmlDocTextNode)(docs.SummaryContents[0])).HtmlDecoded
 			);
+		}
+
+		[Test]
+		public void read_blank_summary_from_class() {
+			var docs = Docs.GetMember("T:TestLibrary1.Class1.Inner");
+			Assert.IsNotNull(docs);
+			Assert.IsNull(docs.SummaryElement);
+			Assert.AreEqual(0, docs.SummaryContents.Count);
+		}
+
+		[Test]
+		public void read_remarks_from_class() {
+			var docs = Docs.GetMember("T:TestLibrary1.Class1.Inner");
+			Assert.IsNotNull(docs);
+			Assert.IsNotNull(docs.RemarksElements);
+			Assert.IsTrue(docs.HasRemarksElements);
+			Assert.AreEqual(1, docs.RemarksElements.Count);
+			Assert.AreEqual("This is just some class.", docs.RemarksElements.First().Node.InnerXml);
+		}
+
+		[Test]
+		public void read_blank_remarks_from_class() {
+			var docs = Docs.GetMember("T:TestLibrary1.Class1.NoRemarks");
+			Assert.IsNotNull(docs);
+			Assert.IsNotNull(docs.RemarksElements);
+			Assert.IsEmpty(docs.RemarksElements);
+		}
+
+		[Test]
+		public void read_summary_and_blank_remarks_from_static_constructor() {
+			var docs = Docs.GetMember("M:TestLibrary1.Class1.#cctor");
+			Assert.IsNotNull(docs);
+			Assert.IsEmpty(docs.RemarksElements);
+			Assert.IsFalse(docs.HasRemarksElements);
+			Assert.IsNotNull(docs.SummaryElement);
+			Assert.AreEqual(1, docs.SummaryContents.Count);
+			Assert.AreEqual("The static constructor.", docs.SummaryContents.First().Node.OuterXml);
+		}
+
+		[Test]
+		public void read_docs_from_parameterized_constructor() {
+			var docs = Docs.GetMember("M:TestLibrary1.Class1.#ctor(System.String)");
+			Assert.IsNotNull(docs);
+			Assert.IsNotNull(docs.SummaryElement);
+			Assert.AreEqual("The instance constructor.", docs.SummaryContents[0].Node.OuterXml);
+			Assert.IsNotNull(docs.RemarksElements);
+			Assert.AreEqual("A remark.", docs.RemarksElements[0].Node.InnerXml);
+			Assert.IsNotNull(docs.GetParameterSummary("crap"));
+			Assert.AreEqual("Whatever.", docs.GetParameterSummary("crap").Node.InnerXml);
+			Assert.IsTrue(docs.HasParameterSummaries);
+			Assert.AreEqual(1, docs.ParameterSummaries.Count);
+		}
+
+		[Test]
+		public void read_two_param_constructor() {
+			var docs = Docs.GetMember("M:TestLibrary1.Class1.#ctor(System.String,System.String)");
+			Assert.IsNotNull(docs);
+			Assert.IsTrue(docs.HasParameterSummaries);
+			Assert.AreEqual(2, docs.ParameterSummaries.Count);
+			Assert.IsNotNull(docs.GetParameterSummary("crap"));
+			Assert.AreEqual("Crap param.", docs.GetParameterSummary("crap").Node.InnerXml);
+			Assert.IsNotNull(docs.GetParameterSummary("dookie"));
+			Assert.AreEqual("Dookie param.", docs.GetParameterSummary("dookie").Node.InnerXml);
+		}
+
+		[Test]
+		public void read_operator_docs() {
+			var docs = Docs.GetMember("M:TestLibrary1.Class1.op_Addition(TestLibrary1.Class1,TestLibrary1.Class1)");
+			Assert.IsNotNull(docs);
+			Assert.AreEqual("The left hand parameter.", docs.GetParameterSummary("a").Node.InnerXml);
+			Assert.AreEqual("The right hand parameter.", docs.GetParameterSummary("b").Node.InnerXml);
+			Assert.IsNotNull(docs.ReturnsElement);
+			Assert.IsTrue(docs.HasReturnsContents);
+			Assert.AreEqual(1, docs.ReturnsContents.Count);
+			Assert.AreEqual("Nope!", docs.ReturnsContents[0].Node.OuterXml);
+		}
+
+		[Test]
+		public void read_indexer_docs() {
+			var docs = Docs.GetMember("P:TestLibrary1.Class1.Item(System.Int32)");
+			Assert.IsNotNull(docs);
+			Assert.AreEqual("Just your average indexer.", docs.SummaryElement.Node.InnerXml);
+			Assert.AreEqual("an index", docs.GetParameterSummary("n").Node.InnerXml);
+			Assert.AreEqual("a number", docs.ReturnsElement.Node.InnerXml);
+			Assert.AreEqual("Some number.", docs.ValueElement.Node.InnerXml);
+		}
+
+		[Test]
+		public void read_const_docs() {
+			var docs = Docs.GetMember("F:TestLibrary1.Class1.MyConst");
+			Assert.IsNotNull(docs);
+			Assert.AreEqual("just a const", docs.SummaryElement.Node.InnerXml);
+			Assert.AreEqual("1", docs.ValueElement.Node.InnerXml);
+		}
+
+		[Test]
+		public void read_delegate_docs() {
+			var docs = Docs.GetMember("T:TestLibrary1.Class1.MyFunc");
+			Assert.IsNotNull(docs);
+			Assert.AreEqual("My delegate.", docs.SummaryElement.Node.InnerXml);
+			Assert.AreEqual("param a", docs.GetParameterSummary("a").Node.InnerXml);
+			Assert.AreEqual("param b", docs.GetParameterSummary("b").Node.InnerXml);
+			Assert.AreEqual("some int", docs.ReturnsElement.Node.InnerXml);
+		}
+
+		[Test]
+		public void read_event_docs() {
+			var docs = Docs.GetMember("E:TestLibrary1.Class1.DoStuff");
+			Assert.IsNotNull(docs);
+			Assert.AreEqual("My event!", docs.SummaryElement.Node.InnerXml);
+		}
+
+		[Test]
+		public void read_exception_from_method() {
+			var docs = Docs.GetMember("M:TestLibrary1.Class1.op_Addition(TestLibrary1.Class1,TestLibrary1.Class1)");
+			Assert.IsNotNull(docs);
+			Assert.AreEqual(2, docs.ExceptionElements.Count);
+			Assert.AreEqual("This is not implemented.", docs.ExceptionElements[0].Node.InnerXml);
+			Assert.That(docs.ExceptionElements.Select(x => x.CRef), Has.All.EqualTo("T:System.NotImplementedException"));
 		}
 
 	}
