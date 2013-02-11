409be475a3fe3bdc012e369852604979d1c5622e
Aaron Dandy <aaron.dandy@live.com>
2013-02-11 00:29:57 -0700

    renamed the core cecil project

--- src/DandyDoc.Core.Cecil/Properties/AssemblyInfo.cs
+++ src/DandyDoc.Core.Cecil/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DandyDoc.Cecil.Core")]
+[assembly: AssemblyTitle("DandyDoc.Core.Cecil")]
 [assembly: AssemblyDescription("Dandy Doc core Cecil assembly.")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
