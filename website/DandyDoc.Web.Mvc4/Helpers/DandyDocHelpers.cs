3d83f27ceb1193c5b7be0a4d23d53346f44635a3
Aaron Dandy <aaron.dandy@live.com>
2013-04-15 22:23:56 -0600

    some XML doc to HTML conversions

--- website/DandyDoc.Web.Mvc4/Helpers/DandyDocHelpers.cs
+++ website/DandyDoc.Web.Mvc4/Helpers/DandyDocHelpers.cs
@@ -5,11 +5,25 @@ using System.Web.Routing;
 using DandyDoc.CodeDoc;
 using System.Text;
 using System.Web.Mvc.Html;
+using DandyDoc.XmlDoc;
 
 namespace DandyDoc.Web.Mvc4.Helpers
 {
     public static class DandyDocHelpers
     {
+
+        public static MvcHtmlString CRefActionLink(this HtmlHelper helper, string text, string cRef){
+            return helper.ActionLink(text, "Api", new { cRef });
+        }
+
+        public static MvcHtmlString CRefActionLink(this HtmlHelper helper, MvcHtmlString innerHtml, string cRef){
+            var linkBuilder = new TagBuilder("a");
+            var urlHelper = new UrlHelper(helper.ViewContext.RequestContext);
+            linkBuilder.MergeAttribute("href", urlHelper.Action("Api", new { cRef }).ToString());
+            if(innerHtml != null)
+                linkBuilder.InnerHtml = innerHtml.ToString();
+            return new MvcHtmlString(linkBuilder.ToString());
+        }
 
         public static MvcHtmlString CodeDocEntityTable(this HtmlHelper helper, IEnumerable<ICodeDocEntity> entities, object tableTagAttributes = null) {
             var builder = new TagBuilder("table");
@@ -17,25 +31,97 @@ namespace DandyDoc.Web.Mvc4.Helpers
             if(tableTagAttributes != null)
                 builder.MergeAttributes(new RouteValueDictionary(tableTagAttributes));
 
-            var rowHtmlBuilder = new StringBuilder();
+            var tableInnerHtmlBuilder = new StringBuilder("<thead><tr><th><i class=\"icon-info-sign\"></i></th><th>Name</th><th>Description</th></tr></thead><tbody>");
             foreach(var entity in entities){
-                rowHtmlBuilder.Append("<tr><td>");
+                tableInnerHtmlBuilder.Append("<tr><td>");
                 // TODO: flair
-                rowHtmlBuilder.Append("</td><td>");
-                rowHtmlBuilder.Append(helper.ActionLink(entity.ShortName, "Api", new { cRef=entity.CRef.FullCRef}));
-                rowHtmlBuilder.Append("</td><td>");
-                // TODO: summry
-                rowHtmlBuilder.Append("</td></tr>");
-            }
+                tableInnerHtmlBuilder.Append("</td><td>");
 
-            builder.InnerHtml = String.Concat(
-                "<thead><tr><th><i class=\"icon-info-sign\"></i></th><th>Name</th><th>Description</th></tr></thead>",
-                "<tbody>",
-                rowHtmlBuilder.ToString(),
-                "</tbody>"
-                );
+                tableInnerHtmlBuilder.Append(helper.CRefActionLink(entity.ShortName, entity.CRef.FullCRef));
+
+                tableInnerHtmlBuilder.Append("</td><td>");
+                if (entity.HasSummary)
+                    tableInnerHtmlBuilder.Append(helper.XmlDocContainedHtml(entity.Summary));
+
+                tableInnerHtmlBuilder.Append("</td></tr>");
+            }
+            tableInnerHtmlBuilder.Append("</tbody>");
+            builder.InnerHtml = tableInnerHtmlBuilder.ToString();
 
             return new MvcHtmlString(builder.ToString());
+        }
+
+        [Obsolete("This should not exist, instead a SummaryContent property for example would be better.")]
+        public static MvcHtmlString XmlDocContainedHtml(this HtmlHelper helper, XmlDocNode node){
+            if (node.HasChildren)
+                return helper.XmlDocHtml(node.Children);
+            return MvcHtmlString.Empty;
+        }
+
+        public static MvcHtmlString XmlDocHtml(this HtmlHelper helper, IEnumerable<XmlDocNode> nodes){
+            var htmlBuilder = new StringBuilder();
+            foreach (var node in nodes)
+                htmlBuilder.Append(helper.XmlDocHtml(node));
+            return new MvcHtmlString(htmlBuilder.ToString());
+        }
+
+        public static MvcHtmlString XmlDocHtml(this HtmlHelper helper, XmlDocNode node){
+            if (node is XmlDocTextNode)
+                return new MvcHtmlString(node.Node.OuterXml);
+
+            if (node is XmlDocNameElement)
+                return helper.XmlDocHtml((XmlDocNameElement)node);
+
+            if (node is XmlDocCodeElement)
+                return helper.XmlDocHtml((XmlDocCodeElement)node);
+
+            if (node is XmlDocRefElement)
+                return helper.XmlDocHtml((XmlDocRefElement)node);
+
+            throw new NotImplementedException();
+        }
+
+        public static MvcHtmlString XmlDocHtml(this HtmlHelper helper, XmlDocNameElement element) {
+            if("PARAMREF".Equals(element.Name)){
+                return element.HasChildren
+                    ? helper.XmlDocHtml(element.Children)
+                    : new MvcHtmlString("<code>" + (element.TargetName ?? "parameter") + "</code>");
+            }
+
+            return element.HasChildren
+                ? helper.XmlDocHtml(element.Children)
+                : new MvcHtmlString(element.TargetName);
+        }
+
+        public static MvcHtmlString XmlDocHtml(this HtmlHelper helper, XmlDocCodeElement element){
+            if (!element.HasChildren)
+                return MvcHtmlString.Empty;
+
+            var codeTag = element.IsInline
+                ? new TagBuilder("code")
+                : new TagBuilder("pre");
+
+            // TODO: need to do something with the language
+
+            codeTag.InnerHtml = helper.XmlDocHtml(element.Children).ToString();
+            return new MvcHtmlString(codeTag.ToString());
+        }
+
+        private static MvcHtmlString XmlDocHtml(this HtmlHelper helper, XmlDocRefElement element){
+            if(!String.IsNullOrWhiteSpace(element.CRef)){
+                if (element.HasChildren) {
+                    return helper.CRefActionLink(
+                        helper.XmlDocHtml(element.Children),
+                        element.CRef);
+                }
+                else{
+                    // TODO: try to use the repository to get a short name
+                    return helper.CRefActionLink(
+                        new MvcHtmlString(element.CRef),
+                        element.CRef);
+                }
+            }
+            throw new NotImplementedException();
         }
 
     }
