57b744e4aa28dfa54225a09bd8b7f4e5e7a0e9db
Aaron Dandy <aaron.dandy@live.com>
2013-04-16 21:00:11 -0600

    XMLDoc to HTML should be done

--- website/DandyDoc.Web.Mvc4/Helpers/DandyDocHelpers.cs
+++ website/DandyDoc.Web.Mvc4/Helpers/DandyDocHelpers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using DandyDoc.CodeDoc;
@@ -12,14 +13,10 @@ namespace DandyDoc.Web.Mvc4.Helpers
     public static class DandyDocHelpers
     {
 
-        public static MvcHtmlString CRefActionLink(this HtmlHelper helper, string text, string cRef){
-            return helper.ActionLink(text, "Api", new { cRef });
-        }
-
         public static MvcHtmlString CRefActionLink(this HtmlHelper helper, MvcHtmlString innerHtml, string cRef){
             var linkBuilder = new TagBuilder("a");
             var urlHelper = new UrlHelper(helper.ViewContext.RequestContext);
-            linkBuilder.MergeAttribute("href", urlHelper.Action("Api", new { cRef }).ToString());
+            linkBuilder.MergeAttribute("href", urlHelper.Action("Api", new { cRef }));
             if(innerHtml != null)
                 linkBuilder.InnerHtml = innerHtml.ToString();
             return new MvcHtmlString(linkBuilder.ToString());
@@ -37,7 +34,7 @@ namespace DandyDoc.Web.Mvc4.Helpers
                 // TODO: flair
                 tableInnerHtmlBuilder.Append("</td><td>");
 
-                tableInnerHtmlBuilder.Append(helper.CRefActionLink(entity.ShortName, entity.CRef.FullCRef));
+                tableInnerHtmlBuilder.Append(helper.CRefActionLink(new MvcHtmlString(entity.ShortName), entity.CRef.FullCRef));
 
                 tableInnerHtmlBuilder.Append("</td><td>");
                 if (entity.HasSummary)
@@ -78,7 +75,12 @@ namespace DandyDoc.Web.Mvc4.Helpers
             if (node is XmlDocRefElement)
                 return helper.XmlDocHtml((XmlDocRefElement)node);
 
-            throw new NotImplementedException();
+            if (node is XmlDocDefinitionList)
+                return helper.XmlDocHtml((XmlDocDefinitionList)node);
+
+            return node.HasChildren
+                ? helper.XmlDocHtml(node.Children)
+                : MvcHtmlString.Empty;
         }
 
         public static MvcHtmlString XmlDocHtml(this HtmlHelper helper, XmlDocNameElement element) {
@@ -107,22 +109,101 @@ namespace DandyDoc.Web.Mvc4.Helpers
             return new MvcHtmlString(codeTag.ToString());
         }
 
-        private static MvcHtmlString XmlDocHtml(this HtmlHelper helper, XmlDocRefElement element){
+        public static MvcHtmlString XmlDocHtml(this HtmlHelper helper, XmlDocRefElement element){
             if(!String.IsNullOrWhiteSpace(element.CRef)){
                 if (element.HasChildren) {
                     return helper.CRefActionLink(
                         helper.XmlDocHtml(element.Children),
                         element.CRef);
                 }
-                else{
-                    // TODO: try to use the repository to get a short name
-                    return helper.CRefActionLink(
-                        new MvcHtmlString(element.CRef),
-                        element.CRef);
-                }
+                // TODO: try to use the repository to get a short name
+                return helper.CRefActionLink(
+                    new MvcHtmlString(element.CRef),
+                    element.CRef);
             }
-            throw new NotImplementedException();
+            if (!String.IsNullOrWhiteSpace(element.HRef)) {
+                var hrefTagBuilder = new TagBuilder("a");
+                hrefTagBuilder.MergeAttribute("href", element.HRef);
+                hrefTagBuilder.InnerHtml = element.HasChildren
+                    ? helper.XmlDocHtml(element.Children).ToString()
+                    : HttpUtility.HtmlEncode(element.HRef);
+                return new MvcHtmlString(hrefTagBuilder.ToString());
+            }
+            if (!String.IsNullOrWhiteSpace(element.LangWord)) {
+                var href = String.Format(
+                    "http://www.bing.com/search?q={0}+keyword+%2Bmsdn",
+                    element.LangWord);
+                var hrefTagBuilder = new TagBuilder("a");
+                hrefTagBuilder.MergeAttribute("href", href);
+                hrefTagBuilder.InnerHtml = element.HasChildren
+                    ? helper.XmlDocHtml(element.Children).ToString()
+                    : HttpUtility.HtmlEncode(element.LangWord);
+                return new MvcHtmlString(hrefTagBuilder.ToString());
+            }
+
+            return element.HasChildren
+                ? helper.XmlDocHtml(element.Children)
+                : MvcHtmlString.Empty;
         }
+
+        public static MvcHtmlString XmlDocHtml(this HtmlHelper helper, XmlDocDefinitionList element) {
+            if (!element.HasItems)
+                return MvcHtmlString.Empty;
+            if ("NUMBER".Equals(element.ListType, StringComparison.OrdinalIgnoreCase))
+                return helper.XmlDocHtmlAsList(element, "ol");
+            if (String.IsNullOrWhiteSpace(element.ListType) || "BULLET".Equals(element.ListType, StringComparison.OrdinalIgnoreCase))
+                return helper.XmlDocHtmlAsList(element, "ul");
+            return helper.XmlDocHtmlAsTable(element);
+
+        }
+
+        private static MvcHtmlString XmlDocHtmlAsTable(this HtmlHelper helper, XmlDocDefinitionList element) {
+            var tableTag = new TagBuilder("table");
+            tableTag.MergeAttribute("class", "table table-bordered table-condensed");
+            var tableGutsBuilder = new StringBuilder();
+            foreach (var row in element.Items) {
+                if(!row.HasTermContents && !row.HasDescriptionContents)
+                    continue;
+
+                var cellTag = row.IsHeader ? "th" : "td";
+                var cellStartTag = String.Concat('<', cellTag, '>');
+                var cellEndTag = String.Concat("</", cellTag, '>');
+                var rowStart = row.IsHeader ? "<thead><tr>" : "<tr>";
+                var rowEnd = row.IsHeader ? "</tr></thead>" : "</tr>";
+
+                tableGutsBuilder.Append(rowStart + cellStartTag);
+                if (row.HasTermContents)
+                    tableGutsBuilder.Append(helper.XmlDocHtml(row.TermContents));
+                tableGutsBuilder.Append(cellEndTag + cellStartTag);
+                if (row.HasDescriptionContents)
+                    tableGutsBuilder.Append(helper.XmlDocHtml(row.DescriptionContents));
+                tableGutsBuilder.Append(cellEndTag + rowEnd);
+            }
+            tableTag.InnerHtml = tableGutsBuilder.ToString();
+            return new MvcHtmlString(tableTag.ToString());
+        }
+
+        private static MvcHtmlString XmlDocHtmlAsList(this HtmlHelper helper, XmlDocDefinitionList element, string listType) {
+            var listTag = new TagBuilder(listType);
+            var listItemsHtmlBuilder = new StringBuilder();
+            foreach (var item in element.Items) {
+                listItemsHtmlBuilder.Append("<li><dl>");
+                if (item.HasTermContents) {
+                    listItemsHtmlBuilder.Append("<dt>");
+                    listItemsHtmlBuilder.Append(helper.XmlDocHtml(item.TermContents));
+                    listItemsHtmlBuilder.Append("</dt>");
+                }
+                if (item.HasDescriptionContents) {
+                    listItemsHtmlBuilder.Append("<dd>");
+                    listItemsHtmlBuilder.Append(helper.XmlDocHtml(item.DescriptionContents));
+                    listItemsHtmlBuilder.Append("</dd>");
+                }
+                listItemsHtmlBuilder.Append("</dl></li>");
+            }
+            listTag.InnerHtml = listItemsHtmlBuilder.ToString();
+            return new MvcHtmlString(listTag.ToString());
+        }
+
 
     }
 }
