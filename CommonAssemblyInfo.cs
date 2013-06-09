abd7f4fe37c689d4e57f2ad394cd0975be01ebe9
Aaron Dandy <aaron.dandy@live.com>
2013-06-08 23:43:21 -0600

    build changes for 0.5 and hopefully many builds to come

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.4.3.*")]
+[assembly: AssemblyVersion("0.5.0.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ release notes.md
@@ -0,0 +1,5 @@
+Release Notes
+=============
+
+## 0.5.0
+* **Breaking**: Refactor and rename of the libraries
