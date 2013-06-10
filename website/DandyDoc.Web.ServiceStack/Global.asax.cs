bc981ea877edd45d98e7d4671883629ccd80f9bc
Aaron Dandy <aaron.dandy@live.com>
2013-06-09 19:32:59 -0600

    trying to get service stack to work how I want it to

---
+++ website/DandyDoc.Web.ServiceStack/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using ServiceStack.Razor;
+using ServiceStack.WebHost.Endpoints;
+
+namespace DandyDoc.Web.ServiceStack
+{
+    public class Global : HttpApplication
+    {
+
+        public class AppHost : AppHostBase
+        {
+            public AppHost() : base("dandy-doc", typeof(AppHost).Assembly) { }
+
+            public override void Configure(Funq.Container container) {
+                Plugins.Add(new RazorFormat());
+            }
+        }
+
+        protected void Application_Start(object sender, EventArgs e) {
+            new AppHost().Init();
+        }
+
+    }
+}
