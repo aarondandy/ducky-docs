8f7db89d034477c90a589f965f945db63664b99d
Aaron Dandy <aaron.dandy@live.com>
2013-06-18 22:46:55 -0600

    version bump

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.5.0.*")]
+[assembly: AssemblyVersion("0.5.1.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
