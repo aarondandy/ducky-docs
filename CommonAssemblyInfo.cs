e812de0ef2602308de7fdeb85db8367a2900683b
Aaron Dandy <aaron.dandy@live.com>
2013-06-18 23:43:38 -0600

    nuget path fixes

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.5.1.*")]
+[assembly: AssemblyVersion("0.5.2.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
